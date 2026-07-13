155880e5d829c5f0815cba7c21f492f90b07e15f
Spyros Sachmpazidis <123989999+Spyros2004@users.noreply.github.com>
2026-07-13 09:36:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-console.log("Hello from JavaScript! 🚀");
+console.log("Hello from JavaScript!");
 
 // Change the greeting after the page loads
 document.addEventListener("DOMContentLoaded", () => {
     const title = document.querySelector("h1");
 
     title.addEventListener("click", () => {
-        title.textContent = "You clicked me! 🎉";
+        title.textContent = "You clicked!";
     });
 });
